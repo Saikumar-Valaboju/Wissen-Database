2f083e8049b58edfab5d4ec510d9beef4386f31e
FirstDev2 <FirstDev2@gmail.com>
2020-08-05 16:17:54 -0700

    Modifid by TL

---
+++ Ajax-ConnectToDatabase/src/com/wissen/action/F2.java
@@ -0,0 +1,12 @@
+package com.wissen.action;
+
+public class F2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public void m1() {
+		System.out.println("F2.m1()");
+	}
+}
