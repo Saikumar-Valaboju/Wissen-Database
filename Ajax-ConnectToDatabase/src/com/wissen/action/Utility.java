cc73963dcfbaba8b9d6ba4a9a94d3613871d171f
FirstDev2 <FirstDev2@gmail.com>
2020-08-05 14:17:48 -0700

    Import By First Developer

--- Ajax-ConnectToDatabase/src/com/wissen/action/Utility.java
+++ Ajax-ConnectToDatabase/src/com/wissen/action/Utility.java
@@ -4,7 +4,7 @@ public class Utility {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Hello world..I am chitti..Speed one tera heartz memory one giga byte");
 	}
 
 }
