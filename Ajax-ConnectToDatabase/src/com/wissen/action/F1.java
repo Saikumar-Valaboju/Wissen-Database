48738ba8db6b91b1e70302a87dd6c3569ea57da4
FirstDev2 <FirstDev2@gmail.com>
2020-08-05 15:17:00 -0700

    second developer created branch functionality code and modified

---
+++ Ajax-ConnectToDatabase/src/com/wissen/action/F1.java
@@ -0,0 +1,10 @@
+package com.wissen.action;
+
+public class F1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("branch created--branch created functionality");
+	}
+
+}
