2a6011f64327e38f9fe00afcb7a3185dabaa04ea
FirstDev2 <FirstDev2@gmail.com>
2020-08-05 14:57:09 -0700

    personal data and utility added & modified by second developer

--- Ajax-ConnectToDatabase/src/com/wissen/action/Utility.java
+++ Ajax-ConnectToDatabase/src/com/wissen/action/Utility.java
@@ -5,7 +5,7 @@ public class Utility {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world..I am chitti..Speed one tera heartz memory one giga byte");
-		System.out.println("First Developer modying content");
+		System.out.println("second developer modyfying content");
 	}
 
 }
