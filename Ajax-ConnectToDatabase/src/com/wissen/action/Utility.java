4fa1563662c09e68a53358d928a6b6e5b760ed48
FirstDev2 <FirstDev2@gmail.com>
2020-08-08 17:53:26 -0700

    m2 modified by Firstdev1

--- Ajax-ConnectToDatabase/src/com/wissen/action/Utility.java
+++ Ajax-ConnectToDatabase/src/com/wissen/action/Utility.java
@@ -7,5 +7,7 @@ public class Utility {
 		System.out.println("Hello world..I am chitti..Speed one tera heartz memory one giga byte");
 		System.out.println("second developer modyfying content");
 	}
-
+	public void m2() {
+		System.out.println("Utility.m2()");
+	}
 }
